843a92ec75f3c88e6ba7f80a2c649ed92a73242e
Robert Forbes <robert.forbes@pentlandedge.com>
2016-02-04 22:22:35 +0000

    Added skeleton project files.

---
+++ src/coord.erl
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Copyright 2016 Pentland Edge Ltd.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. 
+%% You may obtain a copy of the License at
+%%
+%% http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software 
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+%% License for the specific language governing permissions and limitations 
+%% under the License.
+%%
+-module(coord).
+
+-export([lla_to_ecef/1]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Function to convert from Lat, Lon, Alt to ECEF format
+lla_to_ecef({_Lat,_Lon,_Alt}) ->
+    ok.
