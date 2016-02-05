b2bd77a46d1223907f6d0b6f6333ddce4b9482b5
Robert Forbes <robert.forbes@pentlandedge.com>
2016-02-05 13:00:50 +0000

    First attempt at a function to convert coordinates in Lat,Lon,Alt format to ECEF (Earth Centred Earth Fixed).

--- src/coord.erl
+++ src/coord.erl
@@ -17,7 +17,33 @@
 
 -export([lla_to_ecef/1]).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% WGS84 constants.
+-define(WGS84_A, 6378137).
+-define(WGS84_B, 6356752.31424518).
+
 %% Function to convert from Lat, Lon, Alt to ECEF format
-lla_to_ecef({_Lat,_Lon,_Alt}) ->
-    ok.
+lla_to_ecef({Lat,Lon,Alt}) ->
+
+    % Need to check that this height is from the correct reference.
+    H = Alt,
+
+    Asquared = ?WGS84_A * ?WGS84_A,
+    Bsquared = ?WGS84_B * ?WGS84_B,
+    Esquared = (Asquared - Bsquared) / Asquared, 
+
+    SinLat = math:sin(Lat),
+    CosLat = math:cos(Lat),
+    SinLon = math:sin(Lon),
+    CosLon = math:cos(Lon),
+
+    % Calculate the radius of curvature
+    N = ?WGS84_A / math:sqrt(1 - Esquared * SinLat * SinLat),
+
+    % Calculate the coordinate points.
+    X = (N + H) * CosLat * CosLon,
+    Y = (N + H) * CosLat * SinLon,
+    Z = ((Bsquared/Asquared) * N + H) * SinLat, 
+
+    {X, Y, Z}.
+
+
