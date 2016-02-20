52751810dec55e9213afddb9a7d7743a8b4a4a6c
Robert Forbes <robert.forbes@pentlandedge.com>
2016-02-20 18:27:06 +0000

    First partial implementation of a function to extract the most important parameters from mission and dwell segments for display purposes.

--- src/tgt_stats.erl
+++ src/tgt_stats.erl
@@ -17,7 +17,64 @@
 
 -export([extract/1]).
 
-%% Placeholder function to extract the most useful fields relating to 
-%% targets present in a list of decoded s4607 packets.
+-record(dwell_stats, {
+    mission_time, 
+    dwell_time, 
+    target_report_count,
+    sensor_lat,
+    sensor_lon,
+    sensor_alt,
+    dwell_center_lat, 
+    dwell_center_lon, 
+    dwell_range_he, 
+    dwell_angle_he}).
+    
+-record(stat_acc, {ref_time, dwell_list}).
+
+%% Function to extract the most useful fields relating to targets present in
+%% a list of decoded s4607 packets.
 extract(PacketList) when is_list(PacketList) ->
-    ok.
+    % Only interested in mission and dwell segments.
+    Segs = s4607:get_segments_by_type([mission, dwell], PacketList),
+
+    % Function to operate on each segment.
+    F = fun(Seg, #stat_acc{ref_time = RT, dwell_list = DL} = AccStats) ->
+            SH = segment:get_header(Seg),
+            SegData = segment:get_data(Seg),
+            T = seg_header:get_segment_type(SH),
+            case T of
+                mission ->
+                    % Extract the time from the mission segment and update
+                    % the current reference time.
+                    NewRef = mission:get_time(SegData), 
+                    AccStats#stat_acc{ref_time = NewRef};
+                dwell ->
+                    % Extract the most interesting fields from the dwell 
+                    % and note the most recent reference time from the 
+                    % mission segment.
+                    DS = #dwell_stats{
+                        mission_time = RT,
+                        dwell_time = dwell:get_dwell_time(SegData),
+                        target_report_count = dwell:get_target_report_count(SegData),
+                        sensor_lat = dwell:get_sensor_lat(SegData),
+                        sensor_lon = dwell:get_sensor_lon(SegData),
+                        sensor_alt = dwell:get_sensor_alt(SegData),
+                        dwell_center_lat = dwell:get_dwell_center_lat(SegData), 
+                        dwell_center_lon = dwell:get_dwell_center_lon(SegData), 
+                        dwell_range_he = dwell:get_dwell_range_half_extent(SegData), 
+                        dwell_angle_he = dwell:get_dwell_angle_half_extent(SegData)},
+
+                    % Prepend to the list of dwells
+                    NewDwellList = [DS|DL],
+                    AccStats#stat_acc{dwell_list = NewDwellList}
+            end
+        end,
+
+    InitStats = #stat_acc{ref_time = 0, dwell_list = []}, 
+
+    % Apply the fun over the list of segments.
+    #stat_acc{dwell_list = Dwells} = lists:foldl(F, InitStats, Segs),
+    
+    % Reverse the list to return it to chronological order.
+    lists:reverse(Dwells).
+
