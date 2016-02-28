9a8d4dc31925347172b9f2a86b146b9a1372a43f
Robert Forbes <robert.forbes@pentlandedge.com>
2016-02-28 20:30:18 +0000

    Added rebar skeleton files.

---
+++ src/s4607_extra.erl
@@ -0,0 +1,18 @@
+-module(s4607_extra).
+
+%% s4607_extra: s4607_extra library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
