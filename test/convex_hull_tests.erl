8024bae0f2e0ee9b14864fbecfff9a8c13efaf3b
Robert Forbes <robert.forbes@pentlandedge.com>
2017-01-30 23:27:52 +0000

    Added a test to confirm that the supplied reference parameters are indeed returned to the caller when quickhull2 completes.

--- test/convex_hull_tests.erl
+++ test/convex_hull_tests.erl
@@ -6,7 +6,8 @@
 
 %% Define a test generator for the BCS character set functions.
 convex_hull_test_() ->
-    [is_left_checks(), take_right_checks(), quickhull_check1()].
+    [is_left_checks(), take_right_checks(), quickhull_check1(), 
+     quickhull_check2()].
 
 is_left_checks() ->
     [?_assertEqual(true, convex_hull:is_left({0,0},{0,5},{-1,0})),
@@ -36,6 +37,19 @@ quickhull_check1() ->
     ExpectedHull = [B,E,A,C,D],
     [?_assertEqual(Hull, ExpectedHull)].
 
+%% Test that if we supply a reference parameter we do indeed get the 
+%% references back again.
+quickhull_check2() ->
+    Points = point_list2(),
+    Hull = convex_hull:quickhull2(Points),
+    A = {4,-2,{lata,lona}},
+    B = {-4,1,{latb,lonb}},
+    C = {3,1,{latc,lonc}},
+    D = {-1,2,{latd,lond}},
+    E = {-2,-2,{late,lone}},
+    ExpectedHull = [B,E,A,C,D],
+    [?_assertEqual(Hull, ExpectedHull)].
+
 %% Returns a collection of points clustered around the origin. The hull is 
 %% A, B, C, D, E. P and Q are not part of it.
 point_list1() ->
@@ -46,5 +60,18 @@ point_list1() ->
     E = {-2,-2},
     P = {2,0},
     Q = {-1,-1},
+    [Q,C,P,A,E,D,B].
+
+%% Returns a collection of points clustered around the origin. The hull is 
+%% A, B, C, D, E. P and Q are not part of it.
+%% Similar to point_list1(), but includes a reference parameter.
+point_list2() ->
+    A = {4,-2,{lata,lona}},
+    B = {-4,1,{latb,lonb}},
+    C = {3,1,{latc,lonc}},
+    D = {-1,2,{latd,lond}},
+    E = {-2,-2,{late,lone}},
+    P = {2,0,{latp,lonp}},
+    Q = {-1,-1,{latq,lonq}},
     [Q,C,P,A,E,D,B].
 
