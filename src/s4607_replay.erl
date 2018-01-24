d16110a57388a42cbd9c742c433428099f8286dc
Robert Forbes <robert.forbes@pentlandedge.com>
2018-01-24 14:27:24 +0000

    Added code & test to patch the mission date.

--- src/s4607_replay.erl
+++ src/s4607_replay.erl
@@ -3,11 +3,34 @@
 
 -module(s4607_replay).
 
--export([map_packet/3]).
+-export([map_packet/3, patch_mission_seg/2]).
 
 %% @doc Update any mission and dwell segments in the packet to new dates and
 %% times. Return the updated packet and a note of the last dwell time, which
 %% can be used to set the delay before transmitting the packet.
-map_packet(Packet, _MissionDate, _DwellOffset) ->
+map_packet(Packet, MissionDate, DwellOffset) ->
+    PH = s4607:get_packet_header(Packet),
+    Segs = s4607:get_packet_segments(Packet),
+    F = fun(S) ->
+            patch_segment(S, MissionDate, DwellOffset)
+        end,
+    NewSegs = lists:map(F, Segs),
     LastDwellTime = 0,
-    {Packet, LastDwellTime}.
+    NewPacket = s4607:new_packet(PH, NewSegs),
+    {NewPacket, LastDwellTime}.
+
+patch_segment(Seg, MissionDate, _DwellOffset) ->
+    SH = segment:get_header(Seg),
+    SegData = segment:get_data(Seg),
+    Type = seg_header:get_segment_type(SH),
+    case Type of
+        mission ->
+            NewSegData = patch_mission_seg(SegData, MissionDate),
+            segment:new0(SH, NewSegData);
+        _ ->
+            Seg
+    end.
+
+%% @doc Update the reference date in a mission segment.
+patch_mission_seg(MS, Date) ->
+    mission:set_date(MS, Date).
--- test/s4607_replay_tests.erl
+++ test/s4607_replay_tests.erl
@@ -4,4 +4,26 @@
 
 %% Define a test generator function to run all the tests. 
 s4607_replay_test_() ->
-    [].
+    [patch_mission_checks()].
+
+patch_mission_checks() ->
+    MissionPacket = sample_mission_packet(),
+    Date = {2018, 1, 24},
+    {NewPacket, _} = s4607_replay:map_packet(MissionPacket, Date, 0),
+    [MS] = s4607:get_segments([NewPacket]),
+    SegData = segment:get_data(MS), 
+    NewDate = mission:get_time(SegData),
+    [?_assertEqual(Date, NewDate)].
+
+
+sample_mission_packet() ->
+    MS = mission:new("Drifter 1", "A1234", other, "Build 1", 2016, 2, 5),
+    Seg = segment:new(mission, MS),
+    PL = [{version, {3, 1}}, {nationality, "UK"},
+          {classification, unclassified}, {class_system, "UK"},
+          {packet_code, none}, {exercise_ind, exercise_real},
+          {platform_id, "Plat1"}, {mission_id, 16#11223344},
+          {job_id, 16#55667788}],
+    Gen = s4607:packet_generator(PL),
+    Packet = Gen([Seg]),
+    Packet.
