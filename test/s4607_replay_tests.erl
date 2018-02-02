f5d4525a8378cd1bda78b8eeaf0bb8fd541c2f7d
Robert Forbes <robert.forbes@pentlandedge.com>
2018-02-02 13:38:15 +0000

    Added tests of a multiple dwell packet.

--- test/s4607_replay_tests.erl
+++ test/s4607_replay_tests.erl
@@ -7,7 +7,8 @@
 %% Define a test generator function to run all the tests. 
 s4607_replay_test_() ->
     [patch_mission_seg_data_checks(), patch_dwell_seg_data_checks(), 
-     patch_mission_checks(), patch_dwell_checks(), patch_dwell_checks2()].
+     patch_mission_checks(), patch_dwell_checks(), patch_dwell_checks2(),
+     patch_multi_dwell_checks()].
 
 %% Simple check that the mission segment data record can be updated.
 patch_mission_seg_data_checks() ->
@@ -59,12 +60,35 @@ patch_dwell_checks2() ->
     Replay = s4607_replay:init_replay_state(Date, Offset),
     {NewPacket, _NewReplay} = s4607_replay:update_packet(Packet, Replay, Time),
     [DS] = s4607:get_segments([NewPacket]),
-    SegData = segment:get_data(DS), 
-    NewTime = dwell:get_dwell_time(SegData),
+    DwellData = segment:get_data(DS), 
+    NewTime = dwell:get_dwell_time(DwellData),
     % The expected time is the original time in the dwell segment (1000000)
     % plus the offset.
     ExpectedTime = 1000000 + Offset,
     [?_assertEqual(ExpectedTime, NewTime)].
+
+%% Check the patching of multiple segments in a single packet.
+patch_multi_dwell_checks() ->
+    [Packet] = packet_list:get_list3(),
+    Date = {2018, 2, 3},
+    Time = ?TIME_11AM_MS, 
+    Replay = s4607_replay:init_replay_state(Date),
+    {NewPacket, _NewReplay} = s4607_replay:update_packet(Packet, Replay, Time),
+    [_JD, MS, DS1, DS2] = s4607:get_segments([NewPacket]),
+    DwellData1 = segment:get_data(DS1), 
+    DwellData2 = segment:get_data(DS2), 
+    NewTime1 = dwell:get_dwell_time(DwellData1),
+    NewTime2 = dwell:get_dwell_time(DwellData2),
+    LastDwellTime = s4607_replay:get_last_dwell_time(NewPacket),
+    MSdata = segment:get_data(MS), 
+    NewDate = mission:get_time(MSdata),
+    ExpectedTime1 = ?TIME_11AM_MS,
+    ExpectedTime2 = ?TIME_11AM_MS + 5000,
+    ExpectedLastDwellTime = ExpectedTime2,
+    [?_assertEqual(Date, NewDate),
+     ?_assertEqual(ExpectedTime1, NewTime1),
+     ?_assertEqual(ExpectedTime2, NewTime2),
+     ?_assertEqual(ExpectedLastDwellTime, LastDwellTime)].
 
 sample_mission_packet() ->
     MS = mission:new("Drifter 1", "A1234", other, "Build 1", 2016, 2, 5),
