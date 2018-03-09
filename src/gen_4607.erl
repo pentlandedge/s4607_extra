27a67d8ab94cfc2f35215e0c2445d9dafa47a326
Robert Forbes <robert.forbes@pentlandedge.com>
2018-03-09 16:02:26 +0000

    Added a function to create a function that will calculate the position of a target travelling at a constant speed and bearing at a given time.

--- src/gen_4607.erl
+++ src/gen_4607.erl
@@ -1,16 +1,17 @@
 %% Some routines for generating sample Stanag 4607 packets.
 -module(gen_4607).
 
--export([sample_mission_seg/0, sample_mission_seg/1]).
+-export([sample_mission_seg/0, sample_mission_seg/1, gen_position_fun/4]).
 
 -define(MISSION_PLAN, "Hawk Sim 1").
--define(FlIGHT_PLAN, "FP 1").
+-define(FLIGHT_PLAN, "FP 1").
 -define(PLAT_CONFIG, "Sim v1.00").
 
 %% @doc Generate a mission segment (including segment header) with today's 
 %% date.
 sample_mission_seg() ->
-    {{Year,Month,Day}, _} = calendar:universal_time().
+    {Date, _} = calendar:universal_time(),
+    sample_mission_seg(Date).
 
 %% @doc Generate a mission segment (including segment header) with the 
 %% specified date.
@@ -19,8 +20,17 @@ sample_mission_seg({Year, Month, Day}) ->
                      ?FLIGHT_PLAN, 
                      other, 
                      ?PLAT_CONFIG, 
-                     year, 
-                     month, 
-                     day),
+                     Year, 
+                     Month, 
+                     Day),
     segment:new(mission, MS).
+
+%% @doc Generate a function which can calcuate the position of a given target 
+%% at a specified time. This is based on an initial position, a constant 
+%% speed and bearing. The Haversine formula is used.
+gen_position_fun(Lat, Lon, Bearing, Speed) ->
+    fun(Time) ->
+        Distance = Speed * Time,
+        coord:destination({Lat, Lon}, Bearing, Distance)
+    end.
 
